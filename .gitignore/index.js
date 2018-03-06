1158a486efe0bd99844cd3ca3fe788cf7ece7d5b
OrcDrove <37109686+OrcDrove@users.noreply.github.com>
2018-03-06 15:13:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,25 +1,17 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const client = new Discord.Client();
 
-const adapter = new FileSync('database.json');
-const db = low(adapter);
-
-db.defaults({ histoires: [], xp: []}).write()
 var prefix = ("+")
 
 bot.on('ready', function() {
-    bot.user.setPresence({ game: { name: "[+help] en dévelopement", type: 0}});
-    console.log("Connected");
-});
+    bot.user.setPressence({game: { name: "[+help] en dévelopement", type: 0}});
+    console.log("Connecté")
+})
 
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
-
-    //Système de message/ambed 
+    //Système de message
 
     if (message.content === prefix + "owner"){
         message.reply("Ce bot à été créer par **_Jonathan OrcDrove_**");
@@ -88,34 +80,4 @@ bot.on('message', message => {
         console.log("Embed Mini-YTB Envoyé !")
     }
 
-    //Système XP
-
-    var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb);
-        var userxp = Object.values(userxpdb)
-        console.log(userxp)
-        console.log(`Nombre d'xp : ${userxp[1]}`)
-
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-    if (message.content === prefix + "xp"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle("XP")
-            .setThumbnail("https://cdn.discordapp.com/attachments/402137705879699458/419538459334672385/Logo_Final.png")
-            .setDescription(`Voici ton nombre d'XP ${message.author.username} !`)
-            .setColor('#009933')
-            .addField("Tes xp:", `${xpfinal[1]} xp`)
-            .setFooter(`OneNation Communauty | ${message.author.username}`)
-            .setTimestamp()
-        message.channel.send({embed: xp_embed});
-    }}
-});
+})
